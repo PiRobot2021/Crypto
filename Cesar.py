54bba0d018e3ab16141651b4ba8af68bddb918f5
PiRobot2021 <13771688+PiRobot2021@users.noreply.github.com>
2021-12-07 10:23:12 +0100

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+CESAR CIPHER
 First historical cipher (substitution cipher).
 Obtained by shifting all letters of plaintext by 3 positions on the right.
 This algorithm let define the amplitude of the rotation.
