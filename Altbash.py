47b13a05d540a79ca26ce524b0a01ecdfe5eec64
PiRobot2021 <13771688+PiRobot2021@users.noreply.github.com>
2021-12-08 15:09:18 +0100

    Update Altbash.py

--- Altbash.py
+++ Altbash.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
+
 """
 ALTBASH CIPHER
 Used by Hebrew scribes to encrypt the biblical book of Jeremiah.
 It simply reverses the order of the letters in the alphabet (single substitution).
 
 Key space: 26
-
-Usage:
-to encrypt a plaintext: -e plaintext
-to decrypt through all possible letter rotations: -d ciphertext
 """
 
-from sys import argv
 import string
 
 
@@ -25,20 +21,13 @@ def encrypt_decrypt(text):
     ZA= ''.join(AZ)
 
     rotation_tab= text.maketrans(string.ascii_lowercase, za)
-    reverse= ext.translate(rotation_tab)
+    reverse= text.translate(rotation_tab)
     rotation_tab= text.maketrans(string.ascii_uppercase, ZA)
     print(reverse.translate(rotation_tab))
 
 
 def main():
-    if argv[1] == '-e':
-        text= ' '.join(argv[i] for i in range(2, len(argv)))                
-        encrypt_decrypt(text)
-    elif argv[1] == '-d':
-        cipher= ' '.join(argv[i] for i in range(2, len(argv)))
-        decrypt_decrypt(cipher)
-    else:
-        print('To encrypt a plaintext : Altbash.py -e plaintext')
-        print('To decrypt a cipher: Altbash.py -d ciphertext')
-
+    text= input('Type your text: ')
+    encrypt_decrypt(text)
+    
 main()
