a85c290da11af7095db2e502f4a29f424f9c0cb1
PiRobot2021 <13771688+PiRobot2021@users.noreply.github.com>
2021-12-08 15:24:48 +0100

    Update Affine.py

--- Affine.py
+++ Affine.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
+
 """
 AFFINE CIPHERS
 Class of single-substitution ciphers with key pairs, following the formula.
 Common rotation ciphers (e.g. ROT13) fall in this category.
 
 L * a + b (mod N)
-
 L = letter of plaintext
 N = lenght of alphabet
 a = multiplier key, coprime of modulus
 b = adder key
-
-Key space = 182
-Key 1 (modulus coprimes) = 7 (for english alphabet)
-Key 2 = 26
-
-Usage:
-to encrypt a plaintext: -e KEY1 KEY2 plaintext
-to decrypt through all possible keys: -d ciphertext
-to find coprime key1 values to the modulus: -k
 """
 
-from sys import argv
 import string
 from primePy import primes
 from sympy import mod_inverse
@@ -49,11 +39,11 @@ def encrypt(key1, key2, plaintext):
             cipher+= ALPHABET[rot]
         else:
             cipher+= l
-    print(cipher.replace(' ', ''))
+    return cipher.replace(' ', '')
 
 
 def decrypt(cipher):
-    for key1 in coprimes(mod):
+    for key1 in coprimes():
         for key2 in range(mod):
             plaintext= ''
             for l in cipher:
@@ -69,20 +59,17 @@ def decrypt(cipher):
 
 
 def main():
-    if argv[1] == '-e' and len(argv) > 3:                                       # Encrypting with -e option
-        if int(argv[2]) in coprimes(len(string.ascii_lowercase)):               # Cheking if the multiplier key (KEY1) is a coprime of the modulus
-            plaintext= ' '.join(argv[i] for i in range(4, len(argv)))           # If multiple words are given, they are merged into a string
-            encrypt(int(argv[2]), int(argv[3]), plaintext)                      # Encrypt using the KEY1 (argv[2]) and KEY2 (argv[3]), casted as integers. 
-        else:
-            print('KEY1 must be coprime of the modulus, check for possible values with -k')
-    elif argv[1] == '-d':                                                       # Decrypting with -d option
-        cipher= ' '.join(argv[i] for i in range(2, len(argv)))                  # If the ciphertext contains multiple words, they are mergeed into a string
-        decrypt(cipher)                                                         
-    elif argv[1] == '-k':
-        print(coprimes())                                                       # Display possible values of KEY1
-    else:
-        print('To encrypt a plaintext through two integer keys: Affine.py -e KEY1 KEY2 plaintext')
-        print('To decrypt a ciphertext: Affine.py -d ciphertext')
+    # Encryption
+    text= input('Type your text: ')
+    
+    key1= input(f'Chose the first numeric key {coprimes()}: ') 
+    assert(key1 not in coprimes())                                              # Cheking if the multiplier key (KEY1) is a coprime of the modulus
 
+    key2= input('Type the second numeric key: ')
+    cipher= encrypt(int(key1), int(key2), text)                                 # Encrypt using the keys casted as integers 
+    print(f'{cipher}\n')
+    
+    # Decryption
+    #decrypt(cipher)                                                         
 
 main()
