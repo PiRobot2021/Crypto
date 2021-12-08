64a6403d1f6c73047564ebcadd184e348e887aa4
PiRobot2021 <13771688+PiRobot2021@users.noreply.github.com>
2021-12-08 14:47:06 +0100

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -8,7 +8,7 @@ This algorithm let define the amplitude of the rotation.
 Key space = 26
 
 Usage:
-to encrypt a plaintext: -e ROT "plaintext"
+to encrypt a plaintext with a numeric key: -e KEY "plaintext"
 to decrypt through all possible letter rotations: -d "ciphertext"
 """
 
@@ -17,14 +17,16 @@ from sys import argv
 import string
 from collections import deque
 
+az= [l for l in string.ascii_lowercase]
+AZ= [L for L in string.ascii_uppercase]
+
 
 def encrypt(rot, plaintext):
-    shift= deque([l for l in string.ascii_lowercase])
-    shift.rotate(rot)
-    rotation_tab= plaintext.maketrans(string.ascii_lowercase, ''.join(shift))
-    cipher= plaintext.translate(rotation_tab)
-    
-    SHIFT= deque([L for L in string.ascii_uppercase])
+    shift= deque(az)                                                                    # Create a deque of the alphabet
+    shift.rotate(rot)                                                                   # Shift the deque
+    rotation_tab= plaintext.maketrans(string.ascii_lowercase, ''.join(shift))           # Bulid a translation table
+    cipher= plaintext.translate(rotation_tab)                                           # Apply the alphabet shift
+    SHIFT= deque(AZ)
     SHIFT.rotate(rot)
     rotation_tab= cipher.maketrans(string.ascii_uppercase, ''.join(SHIFT))
     print(cipher.translate(rotation_tab))
@@ -32,12 +34,12 @@ def encrypt(rot, plaintext):
     
 def decrypt(cipher):
     for rot in range(26):
-        shift= deque([l for l in string.ascii_lowercase])
+        shift= deque(az)
         shift.rotate(rot)
         rotation_tab= cipher.maketrans(string.ascii_lowercase, ''.join(shift))
         cipher= cipher.translate(rotation_tab)
         
-        SHIFT= deque([L for L in string.ascii_uppercase])
+        SHIFT= deque(AZ)
         SHIFT.rotate(rot)
         rotation_tab= cipher.maketrans(string.ascii_uppercase, ''.join(SHIFT))
         print(rot, '\t', cipher.translate(rotation_tab))
@@ -45,11 +47,14 @@ def decrypt(cipher):
 
 def main():
     if argv[1] == '-e' and len(argv) > 2:
-        encrypt(int(argv[2]), ' '.join(argv[i] for i in range(2, len(argv))))
+        text= ' '.join(argv[i] for i in range(2, len(argv)))                            # Merge multiple words with spaces into one string
+        key= int(argv[2])
+        encrypt(key, text)
     elif argv[1] == '-d':
-        decrypt(' '.join(argv[i] for i in range(2, len(argv))))
+        cipher= ' '.join(argv[i] for i in range(2, len(argv)))                          # Merge multiple words with spaces into one string
+        decrypt(cipher)
     else:
-        print('To encrypt a plaintext with rotation (example 13): Cesar.py -e 13 "plaintext and more plaintext"')
-        print('To decrypt through all possible 26 rotations: Cesar.py -d ciphertext')
+        print('To encrypt a plaintext (example with rotation 13): Cesar.py -e 13 "plaintext and more plaintext"')
+        print('To decrypt through all possible rotations: Cesar.py -d ciphertext')
 
 main()
