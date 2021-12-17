8a676f94aaf5809d860a7780632f9ee9ebb28c79
PiRobot2021 <13771688+PiRobot2021@users.noreply.github.com>
2021-12-17 10:20:46 +0100

    Update Hill.py

--- Hill.py
+++ Hill.py
@@ -1,13 +1,12 @@
 #/usr/bin/env python3
 """
 HILL CIPHER
-
 Substituion cipher that relies on matrix multiplication.
-The plaintext is first converted into integers, with moduls equal to the length of the alphabet used.
-The key is a square matrix of the side length equal to the text length, containing random integers using the same modulus.
-The cipher is obtained by multiplying the vectorized text and the matrix, always within the same modulus.
-The resulting numbers are then converted back to letters.
+All the characters in the plaintext are first converted into integers, with moduls N equal to the length of the alphabet used.
 
+The key is a square matrix with side as long as the text, containing random integers within 0 and modulus N.
+The cipher is obtained by multiplying the vectorized text and the matrix, modulus N.
+The resulting numbers are then converted back to letters.
 """
 
 import string
