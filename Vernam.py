ac4712de666f913f54fe32e59ddf2fe99a44d6ce
PiRobot2021 <13771688+PiRobot2021@users.noreply.github.com>
2021-12-20 13:35:16 +0100

    Create Vernam.py

---
+++ Vernam.py
@@ -0,0 +1,30 @@
+#/usr/bin/env python3
+"""
+VERNAM CIPHER
+It's a historical one-time pad cipher obtained by logical XOR between plaintext and a key with the same length.
+"""
+
+import string
+import random
+#from operator import xor
+
+
+def generate_key(length):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k= length))     # Generate a random string (letters and digits) of the length of the plaintext
+
+
+def encrypt(text, key):
+    return ''.join([chr(text[i] ^ key[i]) for i in range(len(key))]).encode()           # Merge the XOR values into an encoded string 
+        
+
+def main():
+    text= input('Type your text: ')
+    text= text.replace(' ', '').lower()
+
+    key= generate_key(len(text))
+    print(f'Random key: {key}')
+    
+    cipher= encrypt(text.encode(), key.encode())
+    print(f'\nCipher: {cipher}')
+
+main()
