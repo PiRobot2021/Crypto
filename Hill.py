63d9d852b18070326088c6d9fd695b183bc1887e
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 22:14:36 +0100

    Update Hill.py

--- Hill.py
+++ Hill.py
@@ -14,11 +14,11 @@ import numpy as np
 import re
 
 # It can be modified to encrypt more ascii characters
-MOD= len(string.ascii_lowercase)                                                                        # I extracted the modulus value from the code and linked to the alphabet I use
+MOD = len(string.ascii_lowercase)                                                                       # I extracted the modulus value from the code and linked to the alphabet I use
 
 
 def create_random(n):
-    values= np.random.randint(low= 0, high= MOD, size= n * n)                                           # Create a matrix contaning random integers modulus MOD
+    values = np.random.randint(low = 0, high = MOD, size = n * n)                                       # Create a matrix contaning random integers modulus MOD
     return np.array(values).reshape(n, n)                                                               # Reshape the array in a square with side as long as the text
 
 
@@ -29,18 +29,18 @@ def custom_print(matrix, vector, cipher):
 
 
 def encrypt(text):
-    vector= np.array([ord(i) - ord(string.ascii_lowercase[0]) for i in text]).reshape(len(text), 1)     # Convert the plaintext to integers modulus MOD and vectorize it
-    matrix= create_random(len(text))                                                                    # Create a matrix with side of the length of text, containing random integers modulus MOD
-    cipher= np.mod(np.matmul(matrix, vector), MOD)                                                      # Matrix multiplication, the resulting values are also modulus MOD
+    vector = np.array([ord(i) - ord(string.ascii_lowercase[0]) for i in text]).reshape(len(text), 1)    # Convert the plaintext to integers modulus MOD and vectorize it
+    matrix = create_random(len(text))                                                                   # Create a matrix with side of the length of text, containing random integers modulus MOD
+    cipher = np.mod(np.matmul(matrix, vector), MOD)                                                     # Matrix multiplication, the resulting values are also modulus MOD
     custom_print(matrix, vector, cipher)
     return ''.join(chr(i + ord(string.ascii_lowercase[0])) for i in cipher)
 
 
 def main():
-    text= input('Type your text: ')
-    text= re.sub('[\t\s]', '', text.lower())
+    text = input('Type your text: ')
+    text = re.sub('[\t\s]', '', text.lower())
     assert(text.isalpha())                                                                              # The original cipher allows only letters, but it can be easily customized to encrypt all ascii (mod 127)
-    cipher= encrypt(text)
+    cipher = encrypt(text)
     print(f'\nCipher: {cipher}')
 
 if __name__ == '__main__':
