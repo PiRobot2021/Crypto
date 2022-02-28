ce809f43f2df1ca68dcc6bfe9ff0863f3007f8b5
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 21:52:35 +0100

    Update Affine.py

--- Affine.py
+++ Affine.py
@@ -16,28 +16,28 @@ from primePy import primes
 from sympy import mod_inverse
 import random
 
-alphabet= [i for i in string.ascii_lowercase]                                           # I decided to process lower case and upper case letters independently
-ALPHABET= [i for i in string.ascii_uppercase]
-MOD= len(alphabet)                                                                      # The modulus is the length of the alphabet (26 for English)
+alphabet = [i for i in string.ascii_lowercase]                                          # I decided to process lower case and upper case letters independently
+ALPHABET = [i for i in string.ascii_uppercase]
+MOD = len(alphabet)                                                                     # The modulus is the length of the alphabet (26 for English)
 
 
 def coprimes():
-    p= set(primes.between(1, MOD))                                                      # List all the primes lower than the modulus
-    divide_az= {i for i in range(1, MOD) if MOD % i == 0}                               # List all the dividends of the modulus
-    return list(p - divide_az)                                                                # Returns coprimes by logic exclusion between primes lower than modulus and modulus dividends
+    p = set(primes.between(1, MOD))                                                     # List all the primes lower than the modulus
+    divide_az = {i for i in range(1, MOD) if MOD % i == 0}                              # List all the dividends of the modulus
+    return list(p - divide_az)                                                          # Returns coprimes by logic exclusion between primes lower than modulus and modulus dividends
 
 
 def encrypt(key1, key2, plaintext):
-    cipher= ''
+    cipher = ''
     for l in plaintext:
         if l in alphabet:
-            rot= (alphabet.index(l) * key1 + key2) % MOD                                # Rotation (L * a + b) (mod N) for lower case letters
-            cipher+= alphabet[rot]
+            rot = (alphabet.index(l) * key1 + key2) % MOD                               # Rotation (L * a + b) (mod N) for lower case letters
+            cipher += alphabet[rot]
         elif l in ALPHABET:
-            rot= (ALPHABET.index(l) * key1 + key2) % MOD                                # Rotation (L * a + b) (mod N) for upper case letters
-            cipher+= ALPHABET[rot]
+            rot = (ALPHABET.index(l) * key1 + key2) % MOD                               # Rotation (L * a + b) (mod N) for upper case letters
+            cipher += ALPHABET[rot]
         else:
-            cipher+= l
+            cipher += l
     return cipher
 
 
@@ -45,16 +45,16 @@ def decrypt(cipher, solution):
     print('K1\tK2\tTENTATIVE')
     for key1 in coprimes():
         for key2 in range(MOD):
-            plaintext= ''
+            plaintext = ''
             for l in cipher:
                 if l in alphabet:
-                    rot= (mod_inverse(key1, MOD) * (alphabet.index(l) - key2)) % MOD    # Rotation (a^-1 * (L - b) (mod 26) for lower case letters
+                    rot = (mod_inverse(key1, MOD) * (alphabet.index(l) - key2)) % MOD   # Rotation (a^-1 * (L - b) (mod 26) for lower case letters
                     plaintext+= alphabet[rot]
                 elif l in ALPHABET:
-                    rot= (mod_inverse(key1, MOD) * (ALPHABET.index(l) - key2)) % MOD    # Rotation (a^-1 * (L - b) (mod 26) for upper case letters
-                    plaintext+= ALPHABET[rot]
+                    rot = (mod_inverse(key1, MOD) * (ALPHABET.index(l) - key2)) % MOD   # Rotation (a^-1 * (L - b) (mod 26) for upper case letters
+                    plaintext += ALPHABET[rot]
                 else:
-                    plaintext+= l
+                    plaintext += l
             if plaintext == solution:                                                   # This helps visualize the output, just for learning
                 print(f'{key1}\t{key2}\t{plaintext} <-')
             else:
@@ -62,20 +62,20 @@ def decrypt(cipher, solution):
 
 def main():
     # Encryption
-    text= input('Type your text: ')
-    text= text.replace(' ', '')
+    text = input('Type your text: ')
+    text = text.replace(' ', '')
     
-    key1= random.choices(coprimes(), k= 1)[0]                                           # Generate a random key, chosen among coprimes of N
+    key1 = random.choices(coprimes(), k = 1)[0]                                         # Generate a random key, chosen among coprimes of N
     print(f'First random key, coprime of {MOD}: {key1}')
 
-    key2= random.randint(1, 26)                                                         # Generate a random key between 1 and N
+    key2 = random.randint(1, 26)                                                        # Generate a random key between 1 and N
     print(f'Second random key from 1 to {MOD}: {key2}')
     
-    cipher= encrypt(key1, key2, text)                                                   # Encrypt using the keys casted as integers 
+    cipher = encrypt(key1, key2, text)                                                  # Encrypt using the keys casted as integers 
     print(f'\nCipher: {cipher}\n')
     
     # Decryption
-    i= input('Want to try decrypting? [y/n]: ')
+    i = input('Want to try decrypting? [y/n]: ')
     if i == 'y':
         decrypt(cipher, text)                                                           # It's a basic bruteforce
 
