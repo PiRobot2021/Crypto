cf2736dc4cbe89c537a5d7774cab9c6d94848aa0
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 22:02:55 +0100

    Update Geometric.py

--- Geometric.py
+++ Geometric.py
@@ -16,65 +16,65 @@ import random
 import pandas as pd
 
 # Setup values for padding                                              # I have chosen to square the text before encryption to avoid exposing the tail as plaintext
-PADDING_CHAR= '_'                                                       # Select the char for the padding
-RANDOM_PADDING= False                                                   # If False, the text is padded at the end; if True, the text is padded at random positions                                               
-REMOVE_PADDING= True                                                    # If True, all the padding chars are removed after encryption
+PADDING_CHAR = '_'                                                      # Select the char for the padding
+RANDOM_PADDING = False                                                  # If False, the text is padded at the end; if True, the text is padded at random positions                                               
+REMOVE_PADDING = True                                                   # If True, all the padding chars are removed after encryption
 
 
 def squaring(text):
-    text= text.replace(' ', '')                                         # Remove spaces between words
-    side= math.sqrt(len(text))                                          # Approximate the side of the squared text from its length
+    text = text.replace(' ', '')                                        # Remove spaces between words
+    side = math.sqrt(len(text))                                         # Approximate the side of the squared text from its length
     if not side.is_integer():                                           # If the text does not produce a perfect square, adjust the side and add padding
-        side= int(side) + 1
+        side = int(side) + 1
         while len(text) < pow(side, 2):                                 # Pad until the text is long enough for a perfect square
             if RANDOM_PADDING:
-                index= random.randint(0, len(text))                     # Produce a random index in the text
-                text= text[:index] + PADDING_CHAR + text[index:]        # Add the pad
+                index = random.randint(0, len(text))                    # Produce a random index in the text
+                text = text[: index] + PADDING_CHAR + text[index: ]     # Add the pad
             else:
-                text+= PADDING_CHAR
+                text += PADDING_CHAR
     else:
-        side= int(side)
+        side = int(side)
         
-    text= [i for i in text]
-    chunks= [text[side * i : side * i + side] for i in range(len(text[::side]))]    # Chunk the text in pieces as long as the side of the square
-    table= pd.DataFrame(columns=[i for i in range(side)])                           # Create a pandas table as wide as the square side
+    text = [i for i in text]
+    chunks = [text[side * i: side * i + side] for i in range(len(text[: : side]))]  # Chunk the text in pieces as long as the side of the square
+    table = pd.DataFrame(columns=[i for i in range(side)])                          # Create a pandas table as wide as the square side
     for i in range(side):                                                           # Feed the table with the text chunks
-        table.loc[i]= chunks[i]
+        table.loc[i] = chunks[i]
     return table
 
 
 def snake(text):
-    cipher= ''
-    table=squaring(text)                                                # Square the text
+    cipher = ''
+    table = squaring(text)                                                # Square the text
     for y in range(len(table)):
-        if y % 2 == 0:                                                  # If columns are even, proceed downwards
+        if y % 2 == 0:                                                    # If columns are even, proceed downwards
             for x in range(len(table)):
-                cipher+= table.loc[x, y]
-        else:                                                           # If the columns are odd, proceed upwards
+                cipher += table.loc[x, y]
+        else:                                                             # If the columns are odd, proceed upwards
             for x in range(len(table) - 1, -1, -1):
-                cipher+= table.loc[x, y]
+                cipher += table.loc[x, y]
     return cipher
 
 
 def diagonal(text):
-    table= squaring(text)                                               # Square the text
-    cipher= ''
-    for x in range(len(table) - 1, -1, -1):                             # Start from the bottom left corner and proceed upwards
-        y= 0
-        for i in range(x, len(table)):                                  # This process encrypts untill the top left diagonal, then stops
-            cipher+= table.loc[i, y]
-            y+= 1
-    for y in range(1, len(table)):                                      # Conclude the other half of the square, proceed from top left to bottom right
-        x= 0
-        for i in range(y, len(table)):                                  # Concludes at the top right corner
-            cipher+= table.loc[x, i]
-            x+= 1
+    table = squaring(text)                                                # Square the text
+    cipher = ''
+    for x in range(len(table) - 1, -1, -1):                               # Start from the bottom left corner and proceed upwards
+        y = 0
+        for i in range(x, len(table)):                                    # This process encrypts untill the top left diagonal, then stops
+            cipher += table.loc[i, y]
+            y += 1
+    for y in range(1, len(table)):                                        # Conclude the other half of the square, proceed from top left to bottom right
+        x = 0
+        for i in range(y, len(table)):                                    # Concludes at the top right corner
+            cipher += table.loc[x, i]
+            x += 1
     return cipher
 
 
-def geometric_encrypt(text):                                            # I created two examples, one following diagonal path, the other snaking through from left to right
-    cipher= diagonal(text)                                             
-    cipher= snake(cipher)
+def geometric_encrypt(text):                                              # I created two examples, one following diagonal path, the other snaking through from left to right
+    cipher = diagonal(text)                                             
+    cipher = snake(cipher)
     if REMOVE_PADDING:
         return cipher.replace(PADDING_CHAR, '')
     else:
@@ -82,9 +82,9 @@ def geometric_encrypt(text):                                            # I crea
 
     
 def main():
-    text= input('Type your text: ')
+    text = input('Type your text: ')
     # Encryption
-    cipher= geometric_encrypt(text)
+    cipher = geometric_encrypt(text)
     print(f'\nCipher: {cipher}')
 
 if __name__ == '__main__':
