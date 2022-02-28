3d063ec8ee5db2cc4af1b3ef13d84544ce24d55f
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 21:24:59 +0100

    Update Enigma_M3.py

--- Devices/Enigma_M3.py
+++ Devices/Enigma_M3.py
@@ -64,24 +64,24 @@ DEBUG= False
 
 def setup():
     if MANUAL_SETUP:
-        rotor= (1, 2, 3)                                                          # Chose the rotors to use, from 1 to 8. A rotor can be set only once.
-        switches= [('A', 'B')]                                                    # List up to 13 pairs of letters to simulate the plugboard, represented as tuples. A letter can be used only once.                                     
-        start= ('A', 'D', 'U')                                                    # Three letter values, setting the start positions of the rotors.
-        ring_setting= ('A', 'A', 'A')                                             # Internal shift of the ring against the start positions. Each value varies from 0 to 25 (equivalent of A to Z).
-        reflector= 'UKW_B'                                                        # The reflector type: Can be either UKW_B or UKW_C.
+        rotor = (1, 2, 3)                                                         # Chose the rotors to use, from 1 to 8. A rotor can be set only once.
+        switches = [('A', 'B')]                                                   # List up to 13 pairs of letters to simulate the plugboard, represented as tuples. A letter can be used only once.                                     
+        start = ('A', 'D', 'U')                                                   # Three letter values, setting the start positions of the rotors.
+        ring_setting = ('A', 'A', 'A')                                            # Internal shift of the ring against the start positions. Each value varies from 0 to 25 (equivalent of A to Z).
+        reflector = 'UKW_B'                                                       # The reflector type: Can be either UKW_B or UKW_C.
     else:                                                                         # Switch to automatic mode, setup is randomly generated      
-        rotor= tuple(random.sample(range(1, 9), k= 3))                            # Rotors: random sample of three unique values from 1 to 8
-        ring_setting= tuple(random.choices(AZ, k= 3))                             # Ring settings: Tuple of 3 random letters from the alphabet AZ
-        start= tuple(random.choices(AZ, k= 3))                                    # Start positions: Tuple of 3 random letters from the alphabet AZ        
-        reflector= ''.join(random.choices(list(REFLECTOR.keys()), k= 1))          # Reflector type: Chose randomly between the two keys assigned to REFLECTOR dictionary
+        rotor = tuple(random.sample(range(1,9), k = 3))                           # Rotors: random sample of three unique values from 1 to 8
+        ring_setting = tuple(random.choices(AZ, k = 3))                           # Ring settings: Tuple of 3 random letters from the alphabet AZ
+        start = tuple(random.choices(AZ, k = 3))                                  # Start positions: Tuple of 3 random letters from the alphabet AZ        
+        reflector = ''.join(random.choices(list(REFLECTOR), k = 1))               # Reflector type: Chose randomly between the two keys assigned to REFLECTOR dictionary
         
-        switches= []
-        first_letters= random.sample(AZ, k= 10)                                   # Generate 10 random unique letters
-        second_letters= ''.join(set(copy(AZ)).difference(set(first_letters)))     # Create a string the all the remaining letters of the alphabet AZ
+        switches = []
+        first_letters = random.sample(AZ, k = 10)                                 # Generate 10 random unique letters
+        second_letters = ''.join(set(copy(AZ)).difference(set(first_letters)))    # Create a string the all the remaining letters of the alphabet AZ
         for i in first_letters:                                                   # For each letter in the list of 10 randomly generated:
-            j= random.sample(second_letters, k= 1)[0]                             # Chose a random letter from the string of remaining letters
+            j = random.sample(second_letters, k = 1)[0]                           # Chose a random letter from the string of remaining letters
             switches.append((i, j))                                               # Append it to the first letter to form a tuple
-            second_letters= second_letters.replace(f'{j}', '')                    # Remove the appended letter from the remaining letters 
+            second_letters = second_letters.replace(f'{j}', '')                   # Remove the appended letter from the remaining letters 
     assert(check_setup(rotor, switches, start, ring_setting, reflector)) 
     print(f'Rotors: {ROTOR_NAME[rotor[0]]}, {ROTOR_NAME[rotor[1]]}, {ROTOR_NAME[rotor[2]]}')
     print(f'Ring settings: {ring_setting[0]}, {ring_setting[1]}, {ring_setting[2]}')
@@ -107,10 +107,10 @@ def check_setup(rotor, switches, start, ring_setting, reflector):
     for i in ring_setting:
         if type(i) is not str:
             return False
-    if reflector not in REFLECTOR.keys():
+    if reflector not in REFLECTOR:
         return False
     for i in switches:
-        other_switches= copy(switches)
+        other_switches = copy(switches)
         other_switches.remove(i)
         for j in other_switches:
             if i != j and i != j[::-1]:
@@ -120,18 +120,18 @@ def check_setup(rotor, switches, start, ring_setting, reflector):
   
 
 def set_rotors(start, rotor):                                                     # Simulate the rotors as deques. Setup the starting positions by rotating the deques
-    ring_left= [deque(AZ), deque(INNER_RING[rotor[0]])]                           # Each rotor is represented a as list containing the two side of the inner ring, as deques
-    offset= -1 * AZ.index(start[0])                                               # Create an offset from the starting position of the setup function
+    ring_left = [deque(AZ), deque(INNER_RING[rotor[0]])]                          # Each rotor is represented a as list containing the two side of the inner ring, as deques
+    offset = -1 * AZ.index(start[0])                                              # Create an offset from the starting position of the setup function
     ring_left[0].rotate(offset)                                                   # Rotate one side of the rotor
     ring_left[1].rotate(offset)                                                   # Align the other side of the rotor to the same rotation
     
-    ring_centre= [deque(AZ), deque(INNER_RING[rotor[1]])]
-    offset= -1 * AZ.index(start[1])
+    ring_centre = [deque(AZ), deque(INNER_RING[rotor[1]])]
+    offset = -1 * AZ.index(start[1])
     ring_centre[0].rotate(offset)
     ring_centre[1].rotate(offset)
     
-    ring_right= [deque(AZ), deque(INNER_RING[rotor[2]])]
-    offset= -1 * AZ.index(start[2])
+    ring_right = [deque(AZ), deque(INNER_RING[rotor[2]])]
+    offset = -1 * AZ.index(start[2])
     ring_right[0].rotate(offset)
     ring_right[1].rotate(offset)
     
@@ -174,27 +174,27 @@ def Cesar(alphabets, letter, from_offset, to_offset):                           
    
 def Enigma_process(text, rotors, ring_setting, start_positions, reflector_type, switches):
     ring_left, ring_centre, ring_right= set_rotors(start_positions, rotors)                                                         # Set the rotors and the satrt positions
-    alphabet= deque(AZ)                                                                                                             # Create a deque of the alphabet, for the fixed parts of the Enigma
+    alphabet = deque(AZ)                                                                                                            # Create a deque of the alphabet, for the fixed parts of the Enigma
     
-    cipher= ''    
+    cipher = ''    
     for i in text:                                                                                                                  # For each letter in the plaintext:
         ring_left, ring_centre, ring_right= step_rotors(ring_left, ring_centre, ring_right, rotors)                                 # Step the rotors before the encryption
         
-        switched_letter_forward= plugboard(i, switches)                                                                             # Process the plaintext letter thorugh the plugboard
+        switched_letter_forward = plugboard(i, switches)                                                                            # Process the plaintext letter thorugh the plugboard
         
-        rotor_right_forward= Cesar([alphabet, ring_right[1]], switched_letter_forward, 0, ring_setting[2])                          # Process the encrypted letter forward through the rotors
-        rotor_centre_forward= Cesar([ring_right[0], ring_centre[1]], rotor_right_forward, ring_setting[2], ring_setting[1])
-        rotor_left_forward= Cesar([ring_centre[0], ring_left[1]], rotor_centre_forward, ring_setting[1], ring_setting[0])
+        rotor_right_forward = Cesar([alphabet, ring_right[1]], switched_letter_forward, 0, ring_setting[2])                         # Process the encrypted letter forward through the rotors
+        rotor_centre_forward = Cesar([ring_right[0], ring_centre[1]], rotor_right_forward, ring_setting[2], ring_setting[1])
+        rotor_left_forward = Cesar([ring_centre[0], ring_left[1]], rotor_centre_forward, ring_setting[1], ring_setting[0])
 
-        reflector_in= Cesar([ring_left[0], alphabet], rotor_left_forward, ring_setting[0], 0)                                       # Process the encrypted letter through the reflector
-        reflector_out= Cesar([deque(REFLECTOR[reflector_type]), ring_left[0]], reflector_in, 0, ring_setting[0])
+        reflector_in = Cesar([ring_left[0], alphabet], rotor_left_forward, ring_setting[0], 0)                                      # Process the encrypted letter through the reflector
+        reflector_out = Cesar([deque(REFLECTOR[reflector_type]), ring_left[0]], reflector_in, 0, ring_setting[0])
         
-        rotor_left_backward= Cesar([ring_left[1], ring_centre[0]], reflector_out, ring_setting[0], ring_setting[1])                 # Process the encrypted letter backward through the rotors
-        rotor_centre_backward= Cesar([ring_centre[1], ring_right[0]], rotor_left_backward, ring_setting[1], ring_setting[2])
-        rotor_right_backward= Cesar([ring_right[1], alphabet], rotor_centre_backward, ring_setting[2], 0)
+        rotor_left_backward = Cesar([ring_left[1], ring_centre[0]], reflector_out, ring_setting[0], ring_setting[1])                # Process the encrypted letter backward through the rotors
+        rotor_centre_backward = Cesar([ring_centre[1], ring_right[0]], rotor_left_backward, ring_setting[1], ring_setting[2])
+        rotor_right_backward = Cesar([ring_right[1], alphabet], rotor_centre_backward, ring_setting[2], 0)
 
-        switched_letter_backward= plugboard(rotor_right_backward, switches)                                                         # Process the encrypted letter through the plugboard
-        cipher+= switched_letter_backward                                                                                           # Attache the encrypted letter to the ciphertext
+        switched_letter_backward = plugboard(rotor_right_backward, switches)                                                        # Process the encrypted letter through the plugboard
+        cipher += switched_letter_backward                                                                                          # Attache the encrypted letter to the ciphertext
 
         if DEBUG:
             print(f'Keyboard input: {i}')
@@ -217,23 +217,23 @@ def Enigma_process(text, rotors, ring_setting, start_positions, reflector_type, 
     assert(text.isalpha())
 
 
-def prep_text(text):                                                            # Replace common puctuation with letters
-    text= text.replace(' ', 'X')
-    text= text.replace(',', 'QQ')
-    text= text.replace('\n', '')
-    text= text.replace('\r', '')
+def prep_text(text):                                                              # Replace common puctuation with letters
+    text = text.replace(' ', 'X')
+    text = text.replace(',', 'QQ')
+    text = text.replace('\n', '')
+    text = text.replace('\r', '')
     for p in punctuation:
-        text= text.replace(p, '')
+        text = text.replace(p, '')
     check_text(text)
     return text
   
   
 def main():
-    text= input('Type your text (no digits): ')
-    text= prep_text(text)
+    text = input('Type your text (no digits): ')
+    text = prep_text(text)
     
-    rotors, ring_setting, start_positions, reflector_type, switches= setup()                                                        # Load the setup of the device
-    cipher= Enigma_process(text.upper(), rotors, ring_setting, start_positions, reflector_type, switches)
+    rotors, ring_setting, start_positions, reflector_type, switches = setup()                                                        # Load the setup of the device
+    cipher = Enigma_process(text.upper(), rotors, ring_setting, start_positions, reflector_type, switches)
     print(f'\nCipher: {cipher}')
     
 
