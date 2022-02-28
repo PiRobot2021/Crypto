3bbfb11c18fd6574d2ec4e75d80f6b77a27d154c
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 22:29:51 +0100

    Update Vernam.py

--- Vernam.py
+++ Vernam.py
@@ -10,7 +10,7 @@ import random
 
 
 def generate_key(length):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k= length))     # Generate a random string (letters and digits) of the length of the plaintext
+    return ''.join(random.choices(string.ascii_letters + string.digits, k = length))    # Generate a random string (letters and digits) of the length of the plaintext
 
 
 def encrypt(text, key):
@@ -18,13 +18,13 @@ def encrypt(text, key):
         
 
 def main():
-    text= input('Type your text: ')
-    text= text.replace(' ', '').lower()
+    text = input('Type your text: ')
+    text = text.replace(' ', '').lower()
 
-    key= generate_key(len(text))
+    key = generate_key(len(text))
     print(f'Random key: {key}')
     
-    cipher= encrypt(text.encode(), key.encode())
+    cipher = encrypt(text.encode(), key.encode())
     print(f'\nCipher: {cipher}')
 
 if __name__ == '__main__':
