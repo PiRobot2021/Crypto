040c3955f71243447c0a821b582f127a814bb47f
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 21:49:26 +0100

    Update ADFGVX.py

--- ADFGVX.py
+++ ADFGVX.py
@@ -21,44 +21,44 @@ import random
 import numpy as np
 import pandas as pd
 
-LEN_KEY= 5                                                                          # Key length used in the random generator for the columnar encryption
-PADDING_CHAR= '_'                                                                   # Used in the columnar encryption to fit the text in a matrix
+LEN_KEY = 5                                                                         # Key length used in the random generator for the columnar encryption
+PADDING_CHAR = '_'                                                                  # Used in the columnar encryption to fit the text in a matrix
 
 def create_square():                                                                # Creates a 6x6 Polybius square containing shuffled lower ascii letters and digits
-    values= [i for i in string.digits + string.ascii_lowercase]
+    values = [i for i in string.digits + string.ascii_lowercase]
     random.shuffle(values)
     return np.char.asarray(values).reshape(6, 6)
 
 
 def find_coords(square, letter):                                                    # Find the coordinates of a plaintext char in the 6x6 Polybius square and convert them
-    ADFGVX= dict(zip([i for i in range(6)], list('ADFGVX')))                        # Dictionary used for conversion of integers to letters ADFGVX
-    coords= np.where(square == letter)
+    ADFGVX = dict(zip(range(6), list('ADFGVX')))                                    # Dictionary used for conversion of integers to letters ADFGVX
+    coords = np.where(square == letter)
     return ADFGVX[coords[0][0]] + ADFGVX[coords[1][0]]
 
 
 def ADFGVX_encrypt(text):                                               
-    square= create_square()
+    square = create_square()
     print(f'\nADFGVX random square:\n{square}')
     return ''.join([find_coords(square, i) for i in text])
                                                                                                 
 
 def to_table(text, key):                                                            # Tables the plaintext before the columnar encryption
-    table= pd.DataFrame(columns= [i for i in range(len(key))])                      # Create empty table, with columns from "0" to the length of the key
+    table = pd.DataFrame(columns= [i for i in range(len(key))])                     # Create empty table, with columns from "0" to the length of the key
     while len(text) % len(key) != 0:                                                # Pad the text and the tail, to fit into the table
-        text+= PADDING_CHAR
-    j= 0
+        text += PADDING_CHAR
+    j = 0
     for i in range(0, len(text), len(key)):                                         # Split the text in chunks as long as the key length, and load them into the rows of the table
-        chunk= [i for i in text[i: i + len(key)]]
-        table.loc[j]= chunk
-        j+= 1
+        chunk = [i for i in text[i: i + len(key)]]
+        table.loc[j] = chunk
+        j += 1
     return table
 
 
 def to_index(key):                                                                  # Sorts and indexes the key for the columnar encryption
-    sorted_key_chars= sorted(key)
-    ascending_int= [i for i in range(len(key))]
-    ordered_key= list(zip(sorted_key_chars, ascending_int))                         # list of tuples containing sorted key chars and growing int values by steps of 1
-    result= []
+    sorted_key_chars = sorted(key)
+    ascending_int = [i for i in range(len(key))]
+    ordered_key = list(zip(sorted_key_chars, ascending_int))                        # list of tuples containing sorted key chars and growing int values by steps of 1
+    result = []
     for x in key:
         for y in ordered_key:
             if y[0] == x:
@@ -70,26 +70,26 @@ def to_index(key):                                                              
 
 def columnar_encrypt(text, key):
     print(f'\nIntermediate cipher: {text}')
-    table= to_table(text, key)                                                      # Table the text
+    table = to_table(text, key)                                                     # Table the text
     print(f'\nSquared intermediate cipher:\n{table}')
     print(f'\nRandom key: {key}')
     key = to_index(key)                                                             # Convert the key from string into sorted tuples containing the ordered values of the key chars
     print(f'Sorted key: {key}')  
-    cipher= ''
+    cipher = ''
     for column in key:
-        cipher+= ''.join(table[column[1]])                                          # Encrypt by proceeding through each column, in the order given by the ranked key chars
+        cipher += ''.join(table[column[1]])                                         # Encrypt by proceeding through each column, in the order given by the ranked key chars
     return cipher.replace(PADDING_CHAR, '')                                         # Remove the padding chars
     
     
 def encrypt(text, key):
-    intermediate_cipher= ADFGVX_encrypt(text)
-    cipher= columnar_encrypt(intermediate_cipher, key)
+    intermediate_cipher = ADFGVX_encrypt(text)
+    cipher = columnar_encrypt(intermediate_cipher, key)
     return cipher
 
 
 def main():
-    text= input('Type your text (only letters, digits and spaces): ')
-    text= text.replace(' ', '').lower()
+    text = input('Type your text (only letters, digits and spaces): ')
+    text = text.replace(' ', '').lower()
     assert(text.isalnum())
 
     key= random.choices(string.ascii_lowercase, k= LEN_KEY)
