e1c6f82a222f17e053ab5e3f1281aab5e78da030
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 22:12:51 +0100

    Update Gronsfeld.py

--- Gronsfeld.py
+++ Gronsfeld.py
@@ -15,14 +15,14 @@ from collections import deque
 SHUFFLED= True
 
 def create_Gronsfeld():                                                                                 # Function to create a Gronsfeld table
-    alphabet= deque(list(string.ascii_lowercase))                                                       # I use a deque to rotate the alphabet
-    table= pd.DataFrame(index= [i for i in range(10)], columns= [i for i in string.ascii_uppercase])    # Create an empty DataFrame in pandas
+    alphabet = deque(list(string.ascii_lowercase))                                                      # I use a deque to rotate the alphabet
+    table = pd.DataFrame(index = range(10), columns = list(string.ascii_uppercase))                     # Create an empty DataFrame in pandas
     for i in range(10):
-        table.loc[i]= alphabet                                                                          # Load the alphabet row by row, rotating it left at each step of the loop
+        table.loc[i] = alphabet                                                                         # Load the alphabet row by row, rotating it left at each step of the loop
         alphabet.rotate(-1)
     if SHUFFLED:
-        table= table.sample(frac= 1)                                                                    # Shuffle the rows
-        table= table.reset_index(drop= True)                                                            # Reset the index and drop the old one, change to drop= False to track the original index
+        table = table.sample(frac = 1)                                                                  # Shuffle the rows
+        table = table.reset_index(drop = True)                                                          # Reset the index and drop the old one, change to drop= False to track the original index
     return table
 
 
@@ -31,22 +31,22 @@ def get_column(table, letter, row):
 
 
 def encrypt(text):
-    Gronsfeld= create_Gronsfeld()
+    Gronsfeld = create_Gronsfeld()
     print(f'Gronsfeld table:\n{Gronsfeld}')
-    cipher= ''
-    j= 0
+    cipher = ''
+    j = 0
     for i in text:
-        cipher+= get_column(Gronsfeld, i, j % 10)                                                       # Obtain column index from Gronsfeld table, rotating through the columns
-        j+= 1
+        cipher += get_column(Gronsfeld, i, j % 10)                                                      # Obtain column index from Gronsfeld table, rotating through the columns
+        j += 1
     return cipher
 
 
 def main():
-    text= input('Type your text (letters only): ')
-    text= text.replace(' ', '').lower()
+    text = input('Type your text (letters only): ')
+    text = text.replace(' ', '').lower()
     assert(text.isalpha())
 
-    cipher= encrypt(text)
+    cipher = encrypt(text)
     print(f'\nCipher: {cipher}')
 
 if __name__ == '__main__':
