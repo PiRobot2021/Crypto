200f2ccbecc08f335728ebac7c586af0f1ca0c50
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 21:54:13 +0100

    Update Bifid.py

--- Bifid.py
+++ Bifid.py
@@ -11,16 +11,16 @@ import string
 
 
 def create_Polybius():
-    az= [i for i in string.ascii_lowercase if i != 'j']                             # "j" is omitted in this case. A variation could be to remove "q" instead.
+    az = [i for i in string.ascii_lowercase if i != 'j']                            # "j" is omitted in this case. A variation could be to remove "q" instead.
     random.shuffle(az)                                                              # Randomize the order of the letters
-    return np.char.array(az, unicode= True).reshape((5, 5))                         # Creating the Polybius square for letters in lower ascii.
+    return np.char.array(az, unicode = True).reshape((5, 5))                        # Creating the Polybius square for letters in lower ascii.
 
 
 def railfence(square, text):
-    x= []
-    y= []
+    x = []
+    y = []
     for i in text:
-        pair= np.where(square == i)
+        pair = np.where(square == i)
         x.append(pair[0][0])
         y.append(pair[1][0])
     return x + y
@@ -29,17 +29,17 @@ def railfence(square, text):
 def encrypt(text):
     square= create_Polybius()
     print(f'\nRandom Polybius square:\n{square}')
-    coords= railfence(square, text)
+    coords = railfence(square, text)
     print(f'\nPolybius coordinates transposed through Railfence:\n{coords}')
     return ''.join([square[coords[i], coords[i + 1]] for i in range(len(coords) - 1)])
 
 
 def main():
-    text= input('Type your text (letters only): ')
-    text= text.replace(' ', '').lower()
+    text = input('Type your text (letters only): ')
+    text = text.replace(' ', '').lower()
     assert(text.isalpha())
 
-    cipher= encrypt(text)
+    cipher = encrypt(text)
     print(f'\nCipher: {cipher}')
 
 if __name__ == '__main__':
