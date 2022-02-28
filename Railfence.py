ba08405098825ed4b1f2348b5c2f9bb41a6d0107
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 22:25:33 +0100

    Update Railfence.py

--- Railfence.py
+++ Railfence.py
@@ -7,24 +7,24 @@ and the encryption proceed alternating through the letters in the two lines.
     
 """
 
-PADDING_CHAR= '_'
+PADDING_CHAR = '_'
 
 def encrypt(text):
     if len(text) % 2 != 0:
-        text+= PADDING_CHAR
-    chunk_one= text[:len(text)//2]
-    chunk_two= text[len(text)//2:]
-    cipher= ''
+        text += PADDING_CHAR
+    chunk_one = text[:len(text)//2]
+    chunk_two = text[len(text)//2:]
+    cipher = ''
     for i in range(len(text) // 2):
-        cipher+= chunk_one[i] + chunk_two[i]
+        cipher += chunk_one[i] + chunk_two[i]
     return cipher
 
     
 def main():
-    text= input('Type your text: ')
+    text = input('Type your text: ')
     
     # Encryption
-    cipher= encrypt(text)
+    cipher = encrypt(text)
     print(f'\nCipher: {cipher}')
 
 if __name__ == '__main__':
