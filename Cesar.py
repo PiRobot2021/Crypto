3691d2dcd6aa2acc8e19b21cc5ea2aad565a141b
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-02-28 21:55:32 +0100

    Update Cesar.py

--- Cesar.py
+++ Cesar.py
@@ -12,26 +12,26 @@ from collections import deque
 
 
 def encrypt(rot, text):
-    az_deque= deque([l for l in string.ascii_lowercase])                            # Create a deque of the lower ascii alphabet and rotate it
+    az_deque = deque([l for l in string.ascii_lowercase])                            # Create a deque of the lower ascii alphabet and rotate it
     az_deque.rotate(rot)
-    new_alphabet= ''.join(az_deque)
+    new_alphabet = ''.join(az_deque)
                                                                   
-    rot_tab= text.maketrans(string.ascii_lowercase, new_alphabet)                   # Bulid a translation table
-    cipher= text.translate(rot_tab)                                                 # Apply the alphabet translation
+    rot_tab = text.maketrans(string.ascii_lowercase, new_alphabet)                   # Bulid a translation table
+    cipher = text.translate(rot_tab)                                                 # Apply the alphabet translation
     
-    AZ_deque= deque([L for L in string.ascii_uppercase])                            # Rotate separately the upper case ascii alphabet
+    AZ_deque = deque([L for L in string.ascii_uppercase])                            # Rotate separately the upper case ascii alphabet
     AZ_deque.rotate(rot)
-    new_alphabet= ''.join(AZ_deque)
+    new_alphabet = ''.join(AZ_deque)
     
-    rot_tab= cipher.maketrans(string.ascii_uppercase, new_alphabet)
+    rot_tab = cipher.maketrans(string.ascii_uppercase, new_alphabet)
     return cipher.translate(rot_tab)
 
 
     
 def decrypt(cipher, solution):
     for rot in range(26):
-        text= encrypt(rot, cipher)
-        if text == solution:                                                        # Easier to visualize, just for the learning
+        text = encrypt(rot, cipher)
+        if text == solution:                                                         # Easier to visualize, just for the learning
             print(rot, '\t', text, '<-')
         else:
             print(rot, '\t', text)
@@ -39,17 +39,17 @@ def decrypt(cipher, solution):
             
 def main():
     # Encryption
-    text= input('Type your text: ')
-    text= text.replace(' ', '')
+    text = input('Type your text: ')
+    text = text.replace(' ', '')
     
-    key= input('Enter a numeric key: ')
+    key = input('Enter a numeric key: ')
     assert(int(key).isdigit())
     
-    cipher= encrypt(int(key), text)
+    cipher = encrypt(int(key), text)
     print(f'\nCipher: {cipher}\n')
     
     # Decryption
-    i= input('Do you want to try decrypting? [y/n]: ')
+    i = input('Do you want to try decrypting? [y/n]: ')
     if i == 'y':
         decrypt(cipher, text)
 
