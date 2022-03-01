6441dfb4f12e62ad88ccbca8eb7f60a2d98a1eb9
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-03-01 08:27:24 +0100

    Update Affine.py

--- Affine.py
+++ Affine.py
@@ -16,14 +16,14 @@ from primePy import primes
 from sympy import mod_inverse
 import random
 
-alphabet = [i for i in string.ascii_lowercase]                                          # I decided to process lower case and upper case letters independently
-ALPHABET = [i for i in string.ascii_uppercase]
+alphabet = list(string.ascii_lowercase)                                                 # I decided to process lower case and upper case letters independently
+ALPHABET = list(string.ascii_uppercase)
 MOD = len(alphabet)                                                                     # The modulus is the length of the alphabet (26 for English)
 
 
 def coprimes():
     p = set(primes.between(1, MOD))                                                     # List all the primes lower than the modulus
-    divide_az = {i for i in range(1, MOD) if MOD % i == 0}                              # List all the dividends of the modulus
+    divide_az = {i for i in range(1, MOD) if (MOD % i) == 0}                            # List all the dividends of the modulus
     return list(p - divide_az)                                                          # Returns coprimes by logic exclusion between primes lower than modulus and modulus dividends
 
 
@@ -31,10 +31,10 @@ def encrypt(key1, key2, plaintext):
     cipher = ''
     for l in plaintext:
         if l in alphabet:
-            rot = (alphabet.index(l) * key1 + key2) % MOD                               # Rotation (L * a + b) (mod N) for lower case letters
+            rot = (alphabet.index(l)*key1 + key2) % MOD                                 # Rotation (L*a + b) (mod N) for lower case letters
             cipher += alphabet[rot]
         elif l in ALPHABET:
-            rot = (ALPHABET.index(l) * key1 + key2) % MOD                               # Rotation (L * a + b) (mod N) for upper case letters
+            rot = (ALPHABET.index(l)*key1 + key2) % MOD                                 # Rotation (L*a + b) (mod N) for upper case letters
             cipher += ALPHABET[rot]
         else:
             cipher += l
@@ -48,10 +48,10 @@ def decrypt(cipher, solution):
             plaintext = ''
             for l in cipher:
                 if l in alphabet:
-                    rot = (mod_inverse(key1, MOD) * (alphabet.index(l) - key2)) % MOD   # Rotation (a^-1 * (L - b) (mod 26) for lower case letters
-                    plaintext+= alphabet[rot]
+                    rot = (mod_inverse(key1, MOD) * (alphabet.index(l)-key2)) % MOD   # Rotation (a^-1 * (L-b) (mod 26) for lower case letters
+                    plaintext += alphabet[rot]
                 elif l in ALPHABET:
-                    rot = (mod_inverse(key1, MOD) * (ALPHABET.index(l) - key2)) % MOD   # Rotation (a^-1 * (L - b) (mod 26) for upper case letters
+                    rot = (mod_inverse(key1, MOD) * (ALPHABET.index(l)-key2)) % MOD   # Rotation (a^-1 * (L-b) (mod 26) for upper case letters
                     plaintext += ALPHABET[rot]
                 else:
                     plaintext += l
@@ -65,7 +65,7 @@ def main():
     text = input('Type your text: ')
     text = text.replace(' ', '')
     
-    key1 = random.choices(coprimes(), k = 1)[0]                                         # Generate a random key, chosen among coprimes of N
+    key1 = random.choices(coprimes(), k=1)[0]                                           # Generate a random key, chosen among coprimes of N
     print(f'First random key, coprime of {MOD}: {key1}')
 
     key2 = random.randint(1, 26)                                                        # Generate a random key between 1 and N
