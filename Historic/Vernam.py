1f4eeb216601416bb1d2eaefe7cb3279915ce0fa
RustyRobot <13771688+RustyRobot77@users.noreply.github.com>
2022-11-28 13:59:47 +0100

    Update Vernam.py

--- Historic/Vernam.py
+++ Historic/Vernam.py
@@ -5,24 +5,23 @@ It's a historical one-time pad cipher obtained by logical XOR between plaintext 
 """
 
 import string
-import random
+import secrets
 #from operator import xor
+
+TEXT = 'Type your text here...'
 
 
 def generate_key(length):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))      # Generate a random string (letters and digits) of the length of the plaintext
+    return ''.join([secrets.choice(string.ascii_letters + string.digits) for i in range(length)])      # Generate a random string (letters and digits) of the length of the plaintext
 
 
 def encrypt(text, key):
-    return ''.join([chr(text[i] ^ key[i]) for i in range(len(key))]).encode()           # Merge the XOR values into an encoded string 
+    return ''.join([chr(text[i] ^ key[i]) for i in range(len(key))]).encode()                          # Merge the XOR values into an encoded string 
         
 
 if __name__ == '__main__':
-    text = input('Type your text: ')
-    text = text.replace(' ', '').lower()
-
+    text = TEXT.replace(' ', '').lower()
     key = generate_key(len(text))
-    print(f'Random key: {key}')
-    
+    print(f'Random key: {key}')    
     enc_text = encrypt(text.encode(), key.encode())
     print(f'\nCiphertext: {enc_text}')
